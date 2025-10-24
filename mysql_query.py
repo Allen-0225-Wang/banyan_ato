f10f5f861ea4898946e528ef618a2c00f951b3b5
Allen-0225-Wang <37998833+Allen-0225-Wang@users.noreply.github.com>
2025-10-24 11:18:02 +0800

    update

--- mysql_query.py
+++ mysql_query.py
@@ -5,16 +5,6 @@ import pandas as pd
 
 class MySQLAlchemyQuery:
     def __init__(self, host: str, user: str, password: str, database: str, port: int = 3306):
-        """
-        使用SQLAlchemy初始化MySQL连接
-        
-        Args:
-            host: 数据库主机地址
-            user: 用户名
-            password: 密码
-            database: 数据库名
-            port: 端口号
-        """
         # 创建连接字符串
         connection_string = f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}"
         
@@ -26,24 +16,11 @@ class MySQLAlchemyQuery:
             max_overflow=10,
             pool_pre_ping=True  # 连接前检查连接是否有效
         )
-        
-        # 创建会话工厂
+
         self.Session = sessionmaker(bind=self.engine)
-        
-        # 元数据
         self.metadata = MetaData()
     
     def query_to_dataframe(self, query: str, params: Optional[Dict] = None) -> pd.DataFrame:
-        """
-        执行查询并返回DataFrame
-        
-        Args:
-            query: SQL查询语句
-            params: 查询参数
-            
-        Returns:
-            pandas DataFrame
-        """
         try:
             if params:
                 df = pd.read_sql(query, self.engine, params=params)
@@ -55,16 +32,6 @@ class MySQLAlchemyQuery:
             return pd.DataFrame()
     
     def execute_raw_query(self, query: str, params: Optional[Dict] = None) -> List[Dict]:
-        """
-        执行原始SQL查询
-        
-        Args:
-            query: SQL查询语句
-            params: 查询参数
-            
-        Returns:
-            结果列表
-        """
         session = self.Session()
         try:
             if params:
@@ -83,15 +50,6 @@ class MySQLAlchemyQuery:
             session.close()
     
     def get_table_info(self, table_name: str) -> pd.DataFrame:
-        """
-        获取表结构信息
-        
-        Args:
-            table_name: 表名
-            
-        Returns:
-            表结构DataFrame
-        """
         query = """
         SELECT 
             COLUMN_NAME,
@@ -107,7 +65,6 @@ class MySQLAlchemyQuery:
 
 # 使用示例
 def sqlalchemy_usage():
-    # 创建连接
     db = MySQLAlchemyQuery(
         host='44.241.205.126',
         user='shenzhen',
@@ -115,7 +72,6 @@ def sqlalchemy_usage():
         database='gbops'
     )
     
-    # 查询数据到DataFrame
     df = db.query_to_dataframe("SELECT * FROM ASHAREEODPRICES WHERE TRADE_DT = %s", {'TRADE_DT': "20251023"})
     print(df.head(5))
 
